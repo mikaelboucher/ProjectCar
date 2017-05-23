3e74af33bb75b12034e1a024fcc166fa37b29274
OliPro007 <OliPro007@gmail.com>
2017-05-22 20:01:57 -0400

    Starting the login form
    A little template for the sign up form. The form and the style are not
    complete.

--- client/app/components/top.component.ts
+++ client/app/components/top.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: "top-component",
-    templateUrl: './app/html/top.html'
+    templateUrl: './app/html/top.html',
+    styleUrls: [ './app/css/top.css' ]
 })
 export class TopComponent { }
