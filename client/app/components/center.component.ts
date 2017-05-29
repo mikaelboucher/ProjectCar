b687253642c8c5fce09dbae59434b51ee14da305
kevin_takla@hotmail.com <kevin_takla@hotmail.com>
2017-05-28 20:57:20 -0400

    mise en place du feature netflix SANS ANIMATION. ca va venir plus tard apres recherche ou que qql me dit/montre comment

--- client/app/components/center.component.ts
+++ client/app/components/center.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: "center-component",
-    templateUrl: './app/html/center.html'
+    templateUrl: './app/html/center.html',
 })
 export class CenterComponent { }
--- client/app/components/centercomponents/primarycomponents/classfields.component.ts
+++ client/app/components/centercomponents/primarycomponents/classfields.component.ts
@@ -27,14 +27,27 @@ export class Classfields {
         if (enter){
             this.classfieldMouseOver = classfield;
             this.indexMouseOver = index;
+            console.log(this.indexMouseOver);
         }else{
             this.classfieldMouseOver = undefined;
             this.indexMouseOver = undefined;
         }
     }
 
+    estMouse(index : number){
+        return index === this.indexMouseOver;
+    }
+
+    limite(index : number, gauche? : boolean) : boolean{
+        let posEsperer = (gauche ? 0 : 3)
+        return index % NB_BOUTON_LIGNE === posEsperer && !this.memeLigne(index);
+    }
+
     memeLigne(index : number) : boolean{
-        console.log(Math.floor(this.indexMouseOver/4) + " - "+ Math.floor(index/4))
-        return Math.floor(this.indexMouseOver/4) === Math.floor(index/4);
+        let estMemeLigne = false;
+        if (this.indexMouseOver || this.indexMouseOver === 0){
+            estMemeLigne = Math.trunc(index/NB_BOUTON_LIGNE) === Math.trunc(this.indexMouseOver/NB_BOUTON_LIGNE);
+        }
+        return estMemeLigne;
     }
  }
