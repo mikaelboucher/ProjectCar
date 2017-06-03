7959c2679aeb873894e0b7faaa198d1d1492a1d9
kevin_takla@hotmail.com <kevin_takla@hotmail.com>
2017-06-03 00:58:22 -0400

    manque une classe

--- client/app/objects/classfield.ts
+++ client/app/objects/classfield.ts
@@ -1,12 +1,19 @@
 import { Currency } from './currency'
 
+const DEFAULT_STATE = "normal";
+const DEFAULT_TAILLE = "default";
+
 export class Classfield {
 
     imageList: string[];
+    private state : string;
+    private taille : string;
 
     constructor(private title: string, private description: string, private price: number, private currency: Currency) {
         this.imageList = [];
         this.imageList.push("http://singervehicledesign.com/wp-content/uploads/2015/07/12-singer-911-montana-540x320.jpg");
+        this.state = DEFAULT_STATE;
+        this.taille = DEFAULT_TAILLE;
     }
 
     getTitle() {
@@ -25,5 +32,20 @@ export class Classfield {
         return this.currency;
     }
 
+    getState(){
+        return this.state;
+    }
+
+    changeState(state : string){
+        this.state = state;
+    }
+
+    getTaille(){
+        return this.taille;
+    }
+
+    setTaille(taille : string){
+        this.taille = taille;
+    }
 
 }
