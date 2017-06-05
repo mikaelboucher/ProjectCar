c87da47d9d0ce8c634d385cd766c138700a3f837
GabCampbellPoly <kenji901@gmail.com>
2017-06-04 22:07:32 -0400

    premiere implementation de mongo avec une liste temporaire cote serveur dans une liste non lie a la db

---
+++ server/app/database.ts
@@ -0,0 +1,32 @@
+const MONGO_INFO = {
+    MONGO_URI: 'mongodb://NirnrootTestUser:testuserpassword@ds157971.mlab.com:57971/nirnrootdb'
+}
+
+
+export module Database{
+    let mongoClient = require('mongodb').MongoClient;
+
+    export function chercherVoitures(){
+        mongoClient.connect(MONGO_INFO.MONGO_URI, (err: any, db: any) => {
+        console.log(err);
+        let col = db.collection("porsche");
+        col.find({}).toArray((err: any, items: any) => {
+            console.log("erreur est :" + err);
+            console.log(items);
+            console.log(items[0].porsche);
+        })
+        });
+}
+
+    export function ajouterVoiture(obj: any){
+        mongoClient.connect(MONGO_INFO.MONGO_URI, (err: any, db: any) => {
+        console.log(err);
+        let col = db.collection("porsche");
+        col.insert(obj).catch( (err : any) => console.log(err));
+
+        })
+    }
+}
+
+
+
