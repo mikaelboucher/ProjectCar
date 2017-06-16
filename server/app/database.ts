dadc5f0daf573529570f62dfc717b8509ab877e5
GabLotus <GabLotus>
2017-06-15 22:50:56 -0400

    fonction createQuery

--- server/app/database.ts
+++ server/app/database.ts
@@ -7,12 +7,13 @@ export module Database{
 
     export async function findCars(properties: any) : Promise<any> {
         mongoClient.connect(MONGO_INFO.MONGO_URI, (err: any, db: any) => {
-        console.log(err);
+        //console.log(err);
+        let query = createQuery(properties);
         let col = db.collection("porsche");
         return col.find({}).toArray((err: any, items: any) => {
-            console.log("erreur est :" + err);
-            console.log(items);
-            console.log(items[0].porsche);
+            //console.log("erreur est :" + err);
+            //console.log(items);
+            //console.log(items[0].porsche);
         });
 
     });
@@ -25,6 +26,52 @@ export module Database{
 			col.insert(obj).catch( (err : any) => console.log(err));
         });
     }
+
+    function createQuery(properties: any): any{
+        let query = {};
+         query['$and']=[];
+         for(var prop in properties){
+             console.log(`${prop}`);
+             let attribute = {}
+             attribute[`${prop}`] = properties[`${prop}`];
+             query["$and"].push(attribute);
+         }
+         console.log(query);
+        return query;
+    }
+
+    /*
+
+router.get('/leaderboard', function(req, res, next){
+    BD.getLeaderBoard().then(leaderboard => {
+        res.status(200);
+        res.json(leaderboard);
+    }, err => {
+        console.log(err);
+        res.status(500);
+        res.end();
+    });
+});
+
+var q = {}; // declare the query object
+  q['$and']=[]; // filter the search by any criteria given by the user
+  if((req.body.learninglanguages).length > 0){ // if the criteria has a value or values
+    q["$and"].push({ learningLanguages: {$in: req.body.learninglanguages.split(",") }}); // add to the query object
+  }
+  if((req.body.spokenlanguages).length > 0){
+    q["$and"].push({ spokenLanguages: {$in: req.body.spokenlanguages.split(",") }});
+  }
+  if((req.body.country).length > 0){
+    q["$and"].push({ country: {$in: req.body.country.split(",") }});
+  }
+  if((req.body.commethod).length > 0){
+    q["$and"].push({ comMethod: {$in: req.body.commethod.split(",") }});
+  }
+
+
+
+ */
+    
 
 }
 
