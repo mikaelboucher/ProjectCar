35385f5bf588c2367765f9241239af5d33dbf9f1
GabCampbellPoly <kenji901@gmail.com>
2017-06-18 21:22:34 -0400

    modification querry

--- server/app/database.ts
+++ server/app/database.ts
@@ -5,15 +5,17 @@ const MONGO_INFO = {
 export module Database{
     let mongoClient = require('mongodb').MongoClient;
 
-    export function findCars(){
+    export async function findCars(properties: any) : Promise<any> {
         mongoClient.connect(MONGO_INFO.MONGO_URI, (err: any, db: any) => {
         console.log(err);
         let col = db.collection("porsche");
-        col.find({}).toArray((err: any, items: any) => {
+
+        return col.find({}).toArray((err: any, items: any) => {
             console.log("erreur est :" + err);
             console.log(items);
             console.log(items[0].porsche);
         });
+
     });
 }
 
@@ -24,7 +26,82 @@ export module Database{
 			col.insert(obj).catch( (err : any) => console.log(err));
         });
     }
+
+    function generateQuerry(properties: any): any{
+
+        let querry: any;
+        querry['$and'] = []
+        properties.filter((element: JSON) => {
+            if (element.parse.name != ("minYear" || "maxYear" || "minMileage" || "maxMileage")
+            && element != (null || undefined)){
+                return element
+            }
+        }).forEach((element: JSON) => {
+
+        } )
+
+
+        return querry;
+    }
+
 }
+
+
+/*
+
+var q = {}; // declare the query object
+  q['$and']=[]; // filter the search by any criteria given by the user
+  if((req.body.learninglanguages).length > 0){ // if the criteria has a value or values
+    q["$and"].push({ learningLanguages: {$in: req.body.learninglanguages.split(",") }}); // add to the query object
+  }
+
+*/
+
+
+/*
+
+
+function onGeneratedRow(columnsResult)
+{
+    var jsonData = {};
+    columnsResult.forEach(function(column)
+    {
+        var columnName = column.metadata.colName;
+        jsonData[columnName] = column.value;
+    });
+    viewData.employees.push(jsonData);
+ }
+
+
+*/
+
+
+/*
+
+contenu d<une recherche
+
+make
+model[]
+yearMax
+yearMin
+generations[]
+mileageMax
+milageMin
+modified
+trim[]
+chassisType[]
+layout[]
+engine[]
+colour[]
+serialized[]
+porscheCertificate
+VIN
+transmission[]
+
+
+
+
+*/
 
 
 
